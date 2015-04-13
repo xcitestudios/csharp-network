e0dd02ed1adfdf8b1e1c3aae3ca93b50aca4bf32
Wade Womersley <wade85@gmail.com>
2015-04-13 12:36:33 +0100

    Added connection reuse option to AMQPConnection.

--- com/xcitestudios/Network/Server/Connection/AMQPConnection.cs
+++ com/xcitestudios/Network/Server/Connection/AMQPConnection.cs
@@ -10,10 +10,11 @@
     /// </summary>
     public class AMQPConnection
     {
-        private static Dictionary<string, object> Connections = new Dictionary<string, object>();
+        private static Dictionary<string, ConnectionFactory> ConnectionFactories = new Dictionary<string, ConnectionFactory>();
+        private static Dictionary<string, IConnection> Connections = new Dictionary<string, IConnection>();
 
         /// <summary>
-        /// Create a connection using the RabbitMQ library
+        /// Create a connection using the RabbitMQ library, a new connection is always returned.
         /// </summary>
         /// <param name="configuration">Configuration for the connection.</param>
         /// <param name="sslServerName">Only used when configuration.SSL is true. Override the expected server name in the SSL certificate, otherwise use configuration.Host</param>
@@ -22,7 +23,36 @@
         /// <returns>RabbitMQ.Client.IConnection</returns>
         public static IConnection createConnectionUsingRabbitMQ(AMQPServerConfiguration configuration, string sslServerName = null, string sslCertificatePath = null, string sslCertificatePassphrase = null)
         {
-            if (!Connections.ContainsKey("RabbitMQ" + configuration.SerializeJSON()))
+            var factory = SetupFactory(configuration, sslServerName, sslCertificatePath, sslCertificatePassphrase);
+
+            return factory.CreateConnection();
+        }
+
+        /// <summary>
+        /// Create a connection using the RabbitMQ library or reuse a connection if it already exists and is open.
+        /// </summary>
+        /// <param name="configuration">Configuration for the connection.</param>
+        /// <param name="sslServerName">Only used when configuration.SSL is true. Override the expected server name in the SSL certificate, otherwise use configuration.Host</param>
+        /// <param name="sslCertificatePath">Only used when configuration.SSL is true. Path to certificate to use for the connection if not in key store</param>
+        /// <param name="sslCertificatePassphrase">Only used when configuration.SSL is true and sslCertificatePath file is specified. Passphrase to decrypt the sslCertificatePath file</param>
+        /// <returns>RabbitMQ.Client.IConnection</returns>
+        public static IConnection createOrReuseConnectionUsingRabbitMQ(AMQPServerConfiguration configuration, string sslServerName = null, string sslCertificatePath = null, string sslCertificatePassphrase = null)
+        {
+            var keyHash = configuration.SerializeJSON();
+
+            if (!Connections.ContainsKey(keyHash) || !Connections[keyHash].IsOpen)
+            {
+                Connections[keyHash] = createConnectionUsingRabbitMQ(configuration, sslServerName, sslCertificatePath, sslCertificatePassphrase);
+            }
+
+            return Connections[keyHash];
+        }
+
+        private static ConnectionFactory SetupFactory(AMQPServerConfiguration configuration, string sslServerName = null, string sslCertificatePath = null, string sslCertificatePassphrase = null)
+        {
+            var keyHash = configuration.SerializeJSON();
+
+            if (!ConnectionFactories.ContainsKey(keyHash))
             {
                 var factory = new ConnectionFactory();
                 factory.HostName = configuration.Host;
@@ -48,11 +78,10 @@
                     }
                 }
 
-                Connections["RabbitMQ" + configuration.SerializeJSON()] = factory;
+                ConnectionFactories[keyHash] = factory;
             }
 
-
-            return (Connections["RabbitMQ" + configuration.SerializeJSON()] as ConnectionFactory).CreateConnection();
+            return ConnectionFactories[keyHash];
         }
     }
 }
